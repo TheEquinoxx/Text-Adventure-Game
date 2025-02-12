bd2e764f0e5cf2ef66446ba324416be54a70e934
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-12 22:44:39 +1000

    Feat: Added Registration Logic

--- Main_Game.py
+++ Main_Game.py
@@ -1,26 +1,74 @@
-Usernames_Passwords = {"Admin":"Password","Corey":"Password"}
+Usernames_Passwords = {}
 
-def Login_Register():
 
+
+def Login():
+    Login_Username_Given = False
+    while Login_Username_Given == False:
+        Login_Username = str(input("Username: "))
+        Login_Result = Usernames_Passwords.get(Login_Username, "NotFound")
+        if Login_Result != "NotFound":
+            Login_Username_Given = True
+
+            Login_Password_Given = False
+            while Login_Password_Given == False:
+                Login_Password = str(input("Password: "))
+                if Login_Password == Login_Result:
+                    print(f"Login Successful, Welcome {Login_Username}!")
+                    Login_Password_Given = True
+                else:
+                    print("Wrong Password")
+        else:
+            print("No Username Found")
+
+
+
+def Register():
+    Register_Username_Given = False
+    while Register_Username_Given == False:
+        Register_Username = str(input("Username: "))
+        Register_Result = Usernames_Passwords.get(Register_Username, "NotFound")
+        if Register_Result == "NotFound":
+            Register_Username_Confirmation = False
+            while Register_Username_Confirmation == False:
+                Register_Username_Confirm = str(input("Username Available, Confirm? Yes or No: ")).lower() 
+                if Register_Username_Confirm == "no":
+                    break
+                elif Register_Username_Confirm == "yes":
+                    Register_Username_Confirmation = True
+                    Register_Username_Given = True
+
+                    Register_Password_Given = False
+                    while Register_Password_Given == False:
+                        Register_Password = str(input("Password: "))
+                        Register_Password_Confirmation = False
+                        while Register_Password_Confirmation == False:
+                            Register_Password_Confirm = str(input("Confirm Password? Yes or No: ")).lower()
+                            if Register_Password_Confirm == "no":
+                                break
+                            elif Register_Password_Confirm == "yes":
+                                Register_Password_Confirmation = True
+                                Register_Password_Given = True
+                            else:
+                                print("invalid Option")
+                else:
+                    print("invalid Option.")
+        else:
+            print("Username not Available, Try Again.")
+
+    Usernames_Passwords.update({Register_Username:Register_Password})
+
+
+
+def Main_Menu():
     print("Login or Register?")
     User = str(input()).lower()
-
     if User == "login":
-        Login_Username_Given = False
-        while Login_Username_Given == False:
-            Login_Username = str(input("Username: "))
-            Login_Result = Usernames_Passwords.get(Login_Username, "NotFound")
-            if Login_Result != "NotFound":
-                Login_Username_Given = True
-                Login_Password_Given = False
-                while Login_Password_Given == False:
-                    Login_Password = str(input("Password: "))
-                    if Login_Password == Login_Result:
-                        print(f"Login Successful, Welcome {Login_Username}!")
-                        Login_Password_Given = True
-                    else:
-                        print("Wrong Password")
-            else:
-                print("No Username Found")
+        Login()
+    elif User == "register":
+        Register()
+        Login()
 
-Login_Register()
+
+
+Main_Menu()
