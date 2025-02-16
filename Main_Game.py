48bd27ebe535807cd856e97fcb761f7fe4365df8
TheEquinoxx <corey.tyrone.339@gmail.com>
2025-02-16 13:35:45 +1000

    Feat: Added login logic
    Desc: Save and Retrieve usernames and password.

--- Main_Game.py
+++ Main_Game.py
@@ -1,68 +1,88 @@
 import json
-
-Usernames_Passwords = {}
+import keyring
 
 
 
 def Login():
-    Login_Username_Given = False
-    while Login_Username_Given == False:
-        Login_Username = str(input("Login Username: "))
-        Login_Result = Usernames_Passwords.get(Login_Username, "NotFound")
-        if Login_Result != "NotFound":
-            Login_Username_Given = True
+    Login_Username_Given = False # loop switch
+    while Login_Username_Given == False: # begin loop
+        Login_Username = str(input("Login Username: ")) # input username
+        with open("Profiles.json") as FileLoad: # open file
+            Content = json.load(FileLoad) # load content of file to content variable
+            if Login_Username in Content: # checks if username exists in content
+                Username_Result = Login_Username # if it does exist save username as result variable
+            else:
+                Username_Result = "NotFound" # if it doesnt exist save NotFound as result variable
+        if Username_Result != "NotFound": # if result doesnt equal NotFound finish loop
+            Login_Username_Given = True # loop switch
 
-            Login_Password_Given = False
-            while Login_Password_Given == False:
-                Login_Password = str(input("Login Password: "))
-                if Login_Password == Login_Result:
-                    print(f"Login Successful, Welcome {Login_Username}!")
-                    Login_Password_Given = True
+            Login_Password_Given = False # loop switch
+            while Login_Password_Given == False: # begin loop
+                Login_Password = str(input("Login Password: ")) # input password
+                Password_Result = keyring.get_password("Game", Login_Username) # load password for username given as result variable
+                if Login_Password == Password_Result: # checks if password given matches stored password
+                    print(f"Login Successful, Welcome {Login_Username}!") # if it does matches finish Login
+                    Login_Password_Given = True # loop switch
                 else:
-                    print("Wrong Password")
+                    print("Wrong Password") # if it doesnt match print wrong password
         else:
-            print("No Username Found")
+            print("No Username Found") # if result does equal NotFound restart loop
 
 
 
 def Register():
-    Register_Username_Given = False
-    while Register_Username_Given == False:
-        Register_Username = str(input("Register Username: "))
-        Register_Result = Usernames_Passwords.get(Register_Username, "NotFound")
-        if Register_Result == "NotFound":
-            Register_Username_Confirmation = False
-            while Register_Username_Confirmation == False:
-                Register_Username_Confirm = str(input("Username Available, Confirm? Yes or No: ")).lower() 
-                if Register_Username_Confirm == "no":
+    Register_Username_Given = False # loop switch
+    while Register_Username_Given == False: # begin loop
+        Register_Username = str(input("Register Username: ")) # input username to register
+        with open("Profiles.json") as FileLoad: # open file
+            Content = json.load(FileLoad) # load content of file to content variable
+            if Register_Username in Content: # checks if username exists in content
+                Register_Result = Register_Username # if it does exist save username as result variable
+            else:
+                Register_Result = "NotFound" # if it doesnt exist save NotFound as result variable
+        if Register_Result == "NotFound": # if result equals not found meaning username is available
+            Register_Username_Confirmation = False # loop switch
+            while Register_Username_Confirmation == False: # begin loop
+                Register_Username_Confirm = str(input("Username Available, Confirm? Y or N: ")).lower() # ask to confirm username
+                if Register_Username_Confirm == "n": # if not confirmed restart loop
                     break
-                elif Register_Username_Confirm == "yes":
-                    Register_Username_Confirmation = True
-                    Register_Username_Given = True
+                elif Register_Username_Confirm == "y": # if confirmed finish loops
+                    Register_Username_Confirmation = True # loop switch
+                    Register_Username_Given = True # loop switch
 
-                    Register_Password_Given = False
-                    while Register_Password_Given == False:
-                        Register_Password = str(input("Register Password: "))
-                        Register_Password_Confirmation = False
-                        while Register_Password_Confirmation == False:
-                            Register_Password_Confirm = str(input("Confirm Password? Yes or No: ")).lower()
-                            if Register_Password_Confirm == "no":
+                    Register_Password_Given = False # loop switch
+                    while Register_Password_Given == False: # begin loop
+                        Register_Password = str(input("Register Password: ")) # input password
+                        Register_Password_Confirmation = False # loop switch
+                        while Register_Password_Confirmation == False: # begin loop
+                            Register_Password_Confirm = str(input("Confirm Password? Y or N: ")).lower() # ask to confirm password
+                            if Register_Password_Confirm == "n": # if not confirmed restart loop
                                 break
-                            elif Register_Password_Confirm == "yes":
-                                Register_Password_Confirmation = True
-                                Register_Password_Given = True
+                            elif Register_Password_Confirm == "y": # if confirmed finish loops
+                                Register_Password_Confirmation = True # loop switch
+                                Register_Password_Given = True # loop switch
                             else:
-                                print("invalid Option")
+                                print("invalid Option") # invalid yes no option
                 else:
-                    print("invalid Option.")
+                    print("invalid Option.") # invalid yes no option
         else:
-            print("Username not Available, Try Again.")
-
-    Usernames_Passwords.update({Register_Username:Register_Password})
+            print("Username not Available, Try Again.") # if result doesnt equal NotFound meaning username isnt available
 
 
 
-def Main_Menu():
+    # Saving Register Username
+    with open("Profiles.json") as FileLoad:
+        FileObj = json.load(FileLoad)
+    FileObj.update({Register_Username:{}})
+    with open("Profiles.json","w") as FileWrite:
+        json.dump(FileObj, FileWrite, indent=4)
+
+    # Saving Register Password
+    keyring.set_password("Game", Register_Username, Register_Password)
+
+
+
+def LoginScreen():
     print("Login or Register?")
     User = str(input()).lower()
     if User == "login":
@@ -73,4 +93,4 @@ def Main_Menu():
 
 
 
-Main_Menu()
+LoginScreen()
